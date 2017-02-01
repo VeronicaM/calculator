28e96650e8a0bf337400763866f1e8b992d483f1
Veronica Mihai <Veronica Mihai>
2017-02-01 10:10:38 +0200

    completed functionality

--- public/dist/main.js
+++ public/dist/main.js
@@ -1,51 +1,82 @@
 $(function () 
  {    
- 	 var term1=0,term2=0,count=0,op="",isSet=false;
+ 	 var count=0,values=[],op="",solved=false,isSetOp=false,isSetTerm =false,term=0;
 	  $(document).ready(function(){
 	     	
 	  	 $('.numbers div:not(.erasers) button').on('click',function(e){
-	  	 	
-	  	 	 if(count==0 || !isSet){
-	  	 	 	  $("#operation")[0].value +=(this.innerHTML);	
-	  	 	 	  term1= parseInt($("#operation")[0].value);
+	  	 
+	  	 	 if((count==0 || !isSetOp) && !solved){
+	  	 	 	 $("#operation")[0].value +=(this.innerHTML);	
 	  	 	  }else{
-	  	 	  	$("#operation")[0].value =(this.innerHTML);	
-	  	 	  	isSet=false;
-	  	 	  }
-	  	 	
+	  	 	  	$("#operation")[0].value =(this.innerHTML);
+	  	 	  	isSetOp =false;
+	  	 	  	solved = false;
+	  	 	  }	
+
+	  	 	isSetTerm = true;
 	  	 });
 
 	  	  $('.erasers button').on('click',function(e){
 	  	 	 
-	  	 	  switch(this.value){
-	  	 	  	case "CE":  
-	  	 	  	case "C":
-	  	 	  	case "X":
-	  	 	  	default: 
-	  	 	  	   $(".display")[0].dataset.value =""; 
-	  	 	  	   $("#operation")[0].value ="";
-	  	 	  	   count=0;
+	  	 	  switch(this.innerHTML){
+	  	 	  	case "CE":   $("#operation")[0].value =""; break;
+	  	 	  	case "C": 
+ 				 $(".display")[0].dataset.value ="";
+ 				 values = [];
+	  	 	  	 $("#operation")[0].value ="";
 	  	 	  	 break; 
+	  	 	  	 default:   var value = $("#operation")[0].value;
+	  	 	  	            $("#operation")[0].value = value.substring(0,value.length-1);  break;
 	  	 	  }
 	  	 });
 
 	  	   $('.operations button').on('click',function(e){
-	  	   	  op = (this.innerHTML);
-	  	   	  var term= $("#operation")[0].value;
-	  	 	  $(".display")[0].dataset.value += term;
-	  	 	  count++;
-	  	 	  isSet =true;
-	  	 	  if(count ==2 ){
-	  	 		 var compute = eval($(".display")[0].dataset.value);
-	  	 		 $("#operation")[0].value = compute;
-	  	 		 $(".display")[0].dataset.value = compute+op; 
-	  	 		 count=0;
-	  	 		 isSet =false;
-	  	 	 }else{
-	  	 	 	 $(".display")[0].dataset.value +=op;
-	  	 	 }	
+	  	   	  switch(this.innerHTML){
+	  	   	  	case '+':
+	  	   	  	case '-': op = this.innerHTML; break;
+	  	   	  	case '×': op = '*'; break;
+	  	   	  	case '÷' : op ='/'; break;  
+	  	   	  }
+	  	   	  
+	  	   	   term= $("#operation")[0].value;
+
+	  	   	   if(this.innerHTML == '='){
+	  	   	   	  values.push(term);	
+		 		  var compute = eval(values.join(""));
+		 		  $("#operation")[0].value = compute;
+	  	   	   	  $(".display")[0].dataset.value = "";
+	  	   	   	  isSetOp =false;
+	  	   	   	  solved =true;
+	  	   	   	  isSetTerm =true;	
+	  	   	   	  count = 0;
+	  	   	   	  values = [];
+	  	   	   }else{
+	  	   	   		if(isSetTerm){ 
+	  	   	  	$(".display")[0].dataset.value += term+this.innerHTML;	
+	  	   	  	  count++;
+		  	 	  isSetOp = true; 
+		  	 	  isSetTerm =false;
+		  	 	  if(count ==2 ){
+		  	 	    values.push(term);	
+		 		    var compute = eval(values.join(""));
+		 		    $("#operation")[0].value = compute;
+		  	 	    count=1;
+ 		 			values = [compute,op];
+		  	 	  }
+		  	 	  else{
+		  	 	  	values.push(term+op);
+		  	 	  }
+
+	  	   	  }
+	  	   	  else{
+	  	   	  	$(".display")[0].dataset.value = $(".display")[0].dataset.value.substring(0,$(".display")[0].dataset.value.length-1)+this.innerHTML;	
+	  	   	  	values[1] = op;
+	  	   	  }
+	  	   }
+	  	   	  
 	  	 });
 
 	  });
 
+	 
 });
--- src/main.js
+++ src/main.js
@@ -1,39 +1,82 @@
 $(function () 
  {    
- 	 var count=0,op="",isSet=false;
+ 	 var count=0,values=[],op="",solved=false,isSetOp=false,isSetTerm =false,term=0;
 	  $(document).ready(function(){
 	     	
 	  	 $('.numbers div:not(.erasers) button').on('click',function(e){
-	  	 	if(op!=="" && count ==1 ){
-	  	 		 var term = $("#operation")[0].value;
-	  	 		 $("#operation")[0].value = eval($(".display")[0].dataset.value+term);
-	  	 		 count=0;
-	  	 	 }else{
-	  	 	 if(count==0){
+	  	 
+	  	 	 if((count==0 || !isSetOp) && !solved){
 	  	 	 	 $("#operation")[0].value +=(this.innerHTML);	
-	  	 	  }
-	  	 	}
+	  	 	  }else{
+	  	 	  	$("#operation")[0].value =(this.innerHTML);
+	  	 	  	isSetOp =false;
+	  	 	  	solved = false;
+	  	 	  }	
+
+	  	 	isSetTerm = true;
 	  	 });
 
 	  	  $('.erasers button').on('click',function(e){
 	  	 	 
-	  	 	  switch(this.value){
-	  	 	  	case "CE":  
-	  	 	  	case "C":
-	  	 	  	case "X":
-	  	 	  	default:  $("#operation")[0].value ="";
+	  	 	  switch(this.innerHTML){
+	  	 	  	case "CE":   $("#operation")[0].value =""; break;
+	  	 	  	case "C": 
+ 				 $(".display")[0].dataset.value ="";
+ 				 values = [];
+	  	 	  	 $("#operation")[0].value ="";
 	  	 	  	 break; 
+	  	 	  	 default:   var value = $("#operation")[0].value;
+	  	 	  	            $("#operation")[0].value = value.substring(0,value.length-1);  break;
 	  	 	  }
 	  	 });
 
 	  	   $('.operations button').on('click',function(e){
-	  	   	  op = (this.innerHTML);
-	  	   	  var term= $("#operation")[0].value;
-	  	 	  $(".display")[0].dataset.value = term1+op;
-	  	 	  count++;
+	  	   	  switch(this.innerHTML){
+	  	   	  	case '+':
+	  	   	  	case '-': op = this.innerHTML; break;
+	  	   	  	case '×': op = '*'; break;
+	  	   	  	case '÷' : op ='/'; break;  
+	  	   	  }
+	  	   	  
+	  	   	   term= $("#operation")[0].value;
 
+	  	   	   if(this.innerHTML == '='){
+	  	   	   	  values.push(term);	
+		 		  var compute = eval(values.join(""));
+		 		  $("#operation")[0].value = compute;
+	  	   	   	  $(".display")[0].dataset.value = "";
+	  	   	   	  isSetOp =false;
+	  	   	   	  solved =true;
+	  	   	   	  isSetTerm =true;	
+	  	   	   	  count = 0;
+	  	   	   	  values = [];
+	  	   	   }else{
+	  	   	   		if(isSetTerm){ 
+	  	   	  	$(".display")[0].dataset.value += term+this.innerHTML;	
+	  	   	  	  count++;
+		  	 	  isSetOp = true; 
+		  	 	  isSetTerm =false;
+		  	 	  if(count ==2 ){
+		  	 	    values.push(term);	
+		 		    var compute = eval(values.join(""));
+		 		    $("#operation")[0].value = compute;
+		  	 	    count=1;
+ 		 			values = [compute,op];
+		  	 	  }
+		  	 	  else{
+		  	 	  	values.push(term+op);
+		  	 	  }
+
+	  	   	  }
+	  	   	  else{
+	  	   	  	$(".display")[0].dataset.value = $(".display")[0].dataset.value.substring(0,$(".display")[0].dataset.value.length-1)+this.innerHTML;	
+	  	   	  	values[1] = op;
+	  	   	  }
+	  	   }
+	  	   	  
 	  	 });
 
 	  });
 
+	 
 });
