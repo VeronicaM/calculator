c46436e46b32336b8f8726a5417350f3cdea7013
Veronica Mihai <Veronica Mihai>
2017-02-02 15:26:17 +0200

    fixed a bug for calculations after the eraser buttons were pressed and prevented more than one dot in the number :)

--- src/main.js
+++ src/main.js
@@ -4,37 +4,69 @@ $(function ()
        $('#operation')[0].value = '';
     }
  	 var count=0,values=[],op="",solved=false,isSetOp=false,isSetTerm =false,term=0;
-	  $(document).ready(function(){
-	     	
-	  	 $('.numbers div:not(.erasers) button').on('click',function(e){
-	  	 	 if(this.innerHTML =="0" && ($("#operation")[0].value.charAt(0) ==="0" || $("#operation")[0].value.charAt(0) ==="")){
-	  	 	 	console.log("cannot start with 0");
-	  	 	 }else{
-	  	 	    if((count==0 || !isSetOp) && !solved){
+	  function numbersAction(value){
+	  	if((count==0 || !isSetOp) && !solved){
 
-	  	 	 	 $("#operation")[0].value +=(this.innerHTML);	
+	  	 	 	 $("#operation")[0].value +=value;	
 		  	 	  }else{
-		  	 	  	$("#operation")[0].value =(this.innerHTML);
+		  	 	  	$("#operation")[0].value =value;
 		  	 	  	isSetOp =false;
 		  	 	  	solved = false;
 		  	 	  }	
 
-		  	 	isSetTerm = true;	
-	  	 	 }
+		  	 	isSetTerm = true;
+	  }
+	  $(document).ready(function(){
+	     
+
+	  	 $('.numbers div:not(.erasers) button').on('click',function(e){
+	  		   switch(this.innerHTML){
+	     	         case ".": if($("#operation")[0].value.match(/\./)){break;}
+	     	                   else{
+	     	                   	  if($("#operation")[0].value ===""){
+	     	                   	  	numbersAction(0+this.innerHTML)
+	     	                   	  }
+	     	                   	  else{
+	     	                   	  	numbersAction(this.innerHTML);
+	     	                   	  }
+	     	                   	  break;
+	     	                   }   
+	     	            
+	     	 		 case "0": 
+	         	    	if($("#operation")[0].value.charAt(0) ==="")
+	         	    	{
+	         	    		break;
+	         	    	}  
+			     	default:
+			     	    numbersAction(this.innerHTML);
+			     	    break;
+			     	   	
+			     }		 	 
+	  	 	 
 	  	 	 
 	  	 });
 
 	  	  $('.erasers button').on('click',function(e){
 	  	 	 
 	  	 	  switch(this.innerHTML){
-	  	 	  	case "CE":   $("#operation")[0].value =""; break;
+	  	 	  	case "CE":   
+	  	 	  	$("#operation")[0].value ="0";
+	  	 	  	 isSetTerm=true; 
+	  	 	  	break;
 	  	 	  	case "C": 
  				 $(".display")[0].dataset.value ="";
  				 values = [];
-	  	 	  	 $("#operation")[0].value ="";
+	  	 	  	 $("#operation")[0].value ="0";
+	  	 	  	 isSetTerm=true;
 	  	 	  	 break; 
 	  	 	  	 default:   var value = $("#operation")[0].value;
-	  	 	  	            $("#operation")[0].value = value.substring(0,value.length-1);  break;
+	  	 	  	        
+	  	 	  	            $("#operation")[0].value = value.substring(0,value.length-1);  
+	  	 	  	            if($("#operation")[0].value === ""){
+	  	 	  	            $("#operation")[0].value = 0; 
+	  	 	  	              isSetTerm=true;
+	  	 	  	            }
+	  	 	  	            break;
 	  	 	  }
 	  	 });
 
@@ -46,12 +78,12 @@ $(function ()
 	  	   	  	case '÷' : op ='/'; break;  
 	  	   	  }
 	  	   	  
-	  	   	   term= $("#operation")[0].value;
+	  	   	   term= $("#operation")[0].value.charAt($("#operation")[0].value.length-1) === "." ? $("#operation")[0].value.substring(0,$("#operation")[0].value.length-1):$("#operation")[0].value;
 
 	  	   	   if(this.innerHTML == '=' &&  $("#operation")[0].value!==""){
 	  	   	   	  values.push(term);	
 		 		  var compute = eval(values.join(""));
-		 		  $("#operation")[0].value = compute;
+		 		  $("#operation")[0].value = +(compute).toFixed(10);
 	  	   	   	  $(".display")[0].dataset.value = "";
 	  	   	   	  isSetOp =false;
 	  	   	   	  solved =true;
@@ -67,7 +99,7 @@ $(function ()
 			  	 	  if(count ==2 ){
 			  	 	    values.push(term);	
 			 		    var compute = eval(values.join(""));
-			 		    $("#operation")[0].value = compute;
+			 		    $("#operation")[0].value = +(compute).toFixed(10);
 			  	 	    count=1;
 	 		 			values = [compute,op];
 			  	 	  }
